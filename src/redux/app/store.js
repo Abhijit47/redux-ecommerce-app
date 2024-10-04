fe1846a76b4be3915aaa6ca46e75ef5ed9193a82
Abhijit47 <akarmakar846@gmail.com>
2024-10-04 09:21:24 +0530

    Turn off the redux devtools

--- src/redux/app/store.js
+++ src/redux/app/store.js
@@ -1,10 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from "../features/cartSlice";
-import productSlice from "../features/productSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import cartSlice from '../features/cartSlice';
+import productSlice from '../features/productSlice';
 
 export const store = configureStore({
   reducer: {
     products: productSlice,
-    carts: cartSlice
-  }
+    carts: cartSlice,
+  },
+  devTools: process.env.NODE_ENV !== 'development',
 });
